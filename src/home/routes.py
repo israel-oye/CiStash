baa9aefaa27c450a3127398915f12f0d773944dc
israel-oye <ioyeboade@gmail.com>
2022-11-02 16:33:06 +0100

    add table

--- src/home/routes.py
+++ src/home/routes.py
@@ -1,12 +1,19 @@
 # from ..home import home_bp
+from typing import SupportsInt
 from flask import current_app, redirect, request, render_template, url_for, Blueprint
 home_bp = Blueprint('home_bp', __name__, template_folder="templates", static_folder="static")
+
 
 @home_bp.get("/")
 def index():
     # current_app.logger.info(request.path.split('/'))
-    
-    return render_template("index.html")
+    lvls = [
+        {'id': 1, 'name': 100},
+        {'id': 2, 'name': 200},
+        {'id': 3, 'name': 300},
+        {'id': 4, 'name': 400}
+    ]
+    return render_template("index.html", levels=lvls)
 
 
 # def get_segment(request): 
