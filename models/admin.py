dc27cced5a0bb2eb8988a5052a6d4a45439ba814
israel-oye <ioyeboade@gmail.com>
2023-01-13 23:24:18 +0100

    create Moderator user model

--- models/admin.py
+++ models/admin.py
@@ -1,4 +1,19 @@
 from extensions import db, UserMixin
 
-class Admin(db.Model, UserMixin):
-    ...
+class Moderator(db.Model, UserMixin):
+    """Uploader, i.e Moderator user model"""
+    id_ = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(500), nullable=False)
+
+    @property
+    def is_authenticated(self):
+        return super().is_authenticated
+
+    @property
+    def is_active(self):
+        return super().is_active
+
+    def get_id(self):
+        return super().get_id()
