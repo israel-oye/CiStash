7dffeafde70cc3588ea2715b263bacd062e56180
israel-oye <ioyeboade@gmail.com>
2023-01-13 17:19:47 +0100

    add register route and change auth blueprint folder

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, flash, render_template, redirect, url_for
 from extensions import login_manager
-from models.admin import Admin
+from models.admin import Moderator
 
-auth_bp = Blueprint("auth_bp", __name__, template_folder="templates", static_folder="static")
+auth_bp = Blueprint("auth_bp", __name__, template_folder="src/templates", static_folder="static")
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Admin.query.get(user_id)
 
-@auth_bp.route("/")
+@auth_bp.route("/login")
 def login():
     return render_template()
+
+@auth_bp.route("/register")
+def register():
+    ...
