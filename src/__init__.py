b49258bccfe5fcfe28cbf4872bbb14c950eb0180
israel-oye <ioyeboade@gmail.com>
2023-01-14 23:21:16 +0100

    addlogin manager extension and user loader

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from extensions import db, migrate
+from extensions import db, migrate, crsf, login_manager
+from models.admin import Moderator
 
 from .home import home_bp
 from .auth import auth_bp
@@ -13,8 +14,14 @@ app = Flask(__name__)
 
 app.config.from_object(DevelopmentConfig)
 
+crsf.init_app(app)
 db.init_app(app)
 migrate.init_app(app)
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Moderator.query.get(int(user_id))
 
 app.register_blueprint(home_bp, url_prefix="/home")
 app.register_blueprint(auth_bp, url_prefix="/auth")
