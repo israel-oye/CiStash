d7a4e7d8c6e2efb13f7b6cf7b8af646bcfc4a72d
israel-oye <ioyeboade@gmail.com>
2023-02-09 17:32:37 +0100

    Implement password-hash compare method

--- models/admin.py
+++ models/admin.py
@@ -1,7 +1,9 @@
 from extensions import db, UserMixin
-
+from passlib.hash import sha256_crypt
+from typing import AnyStr
 class Moderator(db.Model, UserMixin):
     """Uploader, i.e Moderator user model"""
+    query : db.Query
     id_ = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
@@ -17,3 +19,6 @@ class Moderator(db.Model, UserMixin):
 
     def get_id(self):
         return super().get_id()
+
+    def password_is_correct(self, password_candidate: str):
+        return sha256_crypt.verify(self.password, password_candidate)
