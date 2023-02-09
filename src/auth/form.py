1e82f552673a969809853a61aa9dafd1e638ed0f
israel-oye <ioyeboade@gmail.com>
2023-02-09 17:31:01 +0100

    add Register form

--- src/auth/form.py
+++ src/auth/form.py
@@ -1,5 +1,17 @@
-from extensions import FlaskForm, StringField, PasswordField, InputRequired
+from extensions import FlaskForm, StringField, PasswordField, EmailField, InputRequired, ValidationError
+from wtforms import validators
+from models.admin import Moderator
 
-class LoginForm(FlaskForm):
-    username = StringField("username", InputRequired("Please enter a username"))
-    password = PasswordField("password", InputRequired())
+class RegisterForm(FlaskForm):
+    username = StringField("Username", [validators.Length(3, 25, "Username must be at least %(min)d long!")], render_kw={"autofocus": True, "class": "form-control", "placeholder": "Username"})
+    email = EmailField("Email", [InputRequired("Please enter an email!")], render_kw={"class": "form-control", "placeholder": "Email"})
+    password = PasswordField("Password", [InputRequired(), validators.EqualTo("confirm", message="Passwords do not match!")], render_kw={"class": "form-control", "placeholder": "Password"})
+    confirm = PasswordField("Confirm Password", render_kw={"class": "form-control", "placeholder": "Confirm password"})
+
+    def validate_email(self, email):
+        if Moderator.query.filter_by(email.data):
+            raise ValidationError("Account exists for this email address!")
+    
+    def validate_username(self, username):
+        if Moderator.query.filter_by(username.data):
+            raise ValidationError("Username is not available!")
