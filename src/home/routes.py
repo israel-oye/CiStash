467c8c6074808285f18d5ed49e4403489110fae5
israel-oye <ioyeboade@gmail.com>
2023-02-13 18:16:21 +0100

    add page not found error handling

--- src/home/routes.py
+++ src/home/routes.py
@@ -1,9 +1,14 @@
 from typing import SupportsInt
-from flask import current_app, redirect, request, render_template, url_for, Blueprint
+from flask import current_app, flash, redirect, request, render_template, url_for, Blueprint
 from models.level import Level
 
 home_bp = Blueprint('home_bp', __name__, template_folder="src/templates", static_folder="src/static")
 
+
+@home_bp.app_errorhandler(404)
+def page_not_found(error):
+    msg = "404 Error: Page Not Found. It looks like you're lost, but don't worry. Try using the search bar or returning to the homepage to find what you're looking for."
+    return render_template("errors/404.html", msg=msg), 404
 
 @home_bp.get("/")
 def index():
