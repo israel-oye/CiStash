454a6674e48e45f001bf2308426e5eb5bc30671e
israel-oye <ioyeboade@gmail.com>
2023-03-09 23:46:14 +0100

    add Course form and field validate methods

--- src/auth/form.py
+++ src/auth/form.py
@@ -1,6 +1,8 @@
-from extensions import FlaskForm, StringField, PasswordField, EmailField, InputRequired, ValidationError
+from extensions import FlaskForm, StringField, PasswordField, EmailField, SelectField, InputRequired, ValidationError
 from wtforms import validators
 from models.admin import Moderator
+from models.course import Course
+from models.level import LevelEnum
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", [validators.Length(3, 25, "Username must be at least %(min)d long!")], render_kw={"autofocus": True, "class": "form-control", "placeholder": "Username"})
@@ -10,8 +12,46 @@ class RegisterForm(FlaskForm):
 
     def validate_email(self, email):
         if Moderator.query.filter_by(email=email.data).first():
-            raise ValidationError("Account exists for this email address!")
+            raise ValidationError("Account exists for this email address")
     
     def validate_username(self, username):
         if Moderator.query.filter_by(username=username.data).first():
-            raise ValidationError("Username is not available!")
+            raise ValidationError("Username is not available")
+        
+class CourseForm(FlaskForm):
+    levels = SelectField(
+                        "Course Level",
+                        validate_choice=True,
+                        default="Course Level",
+                        choices=[(level.name, level.value) for level in LevelEnum],
+                        render_kw={"class": "form-select", "aria-label": "Select level"}
+                        )
+    course_code = StringField(
+                        "Course Code",
+                        [validators.Length(7, 7, "Please enter a valid course code."),  ],
+                        render_kw={"class": "form-control", "placeholder": "e.g CSC 199"}
+                        )
+    course_title = StringField(
+                        "Course Title",
+                        [InputRequired()],
+                        render_kw={"class": "form-control", "placeholder": "e.g Introduction to Web Development", "aria-placeholder": "e.g Introduction to Web Development"}
+                        )
+    dyna_course_code = SelectField(
+                        'Course Code',
+                        validate_choice=False,
+                        choices=[],
+                        render_kw={"class": "form-select", "aria-label": "Select course code"}
+                        )
+
+    def validate_levels(self, selected_level):
+        if selected_level.data not in [l.value for l in LevelEnum]:
+            raise ValidationError("Please select a valid level.")
+    
+    def validate_course_code(self, course_code_field):
+        if Course.query.filter_by(course_code=course_code_field.data).first():
+            raise ValidationError(f"Oops, {course_code_field.data} has been registered.")
+        
+    def validate_course_title(self, course_title_field):
+        if Course.query.filter_by(course_title=course_title_field.data).first():
+            raise ValidationError(f"It seems like this course has been registered, please check carefully and try again.")
+    
