b495f308a749cb8b7569394bb077a3a0bd4d052e
israel-oye <ioyeboade@gmail.com>
2023-03-09 23:49:01 +0100

    add extension config for login view

--- extensions.py
+++ extensions.py
@@ -1,9 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager, UserMixin, current_user, login_user, login_required
+from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user, login_fresh
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFProtect
-from wtforms import StringField, PasswordField, EmailField, ValidationError
+from wtforms import StringField, PasswordField, EmailField, SelectField, ValidationError
 from wtforms.validators import InputRequired
 
 db = SQLAlchemy()
@@ -12,7 +12,7 @@ login_manager = LoginManager()
 crsf = CSRFProtect()
 
 
-login_manager.blueprint_login_views = {"auth_bp": "login"}
+login_manager.blueprint_login_views = {"moderator": "auth_bp.login"}
 login_manager.login_message = "Unauthorized access!"
 login_manager.login_message_category = "danger"
 login_manager.needs_refresh_message = (u"Session timedout, please re-login")
