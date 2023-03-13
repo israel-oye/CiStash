498b4e8305e4c024941e24499e39b2c7d52e01d1
israel-oye <ioyeboade@gmail.com>
2023-03-13 21:47:37 +0100

    patch form handler errors

--- src/static/assets/form-handler.js
+++ src/static/assets/form-handler.js
@@ -47,8 +47,9 @@ async function submitForm(formData) {
         const data = await response.json();
 
         if (response.status === 200) {
-            const alertString = `<div class="alert alert-success mt-4 mx-5 py-2 text-center fw-bold" role="alert">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:"> <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg> 
+            const alertString = `<div class="alert  alert-success mt-4 mx-5 text-center fw-bold alert-dismissible fade show" role="alert">
+                                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                     <div>
                                         ${data.message}
                                     </div>
@@ -57,7 +58,7 @@ async function submitForm(formData) {
             const parser = new DOMParser();
             const doc = parser.parseFromString(alertString, "text/html");
             const alertElem = doc.querySelector('div')
-            const formContainerElem = doc.querySelector('#form-container')
+            const formContainerElem = document.querySelector('#form-container')
 
             formContainerElem.insertAdjacentElement("beforebegin", alertElem);
         } else if (response.status === 400) {
@@ -66,21 +67,22 @@ async function submitForm(formData) {
             //field_errors = {'fieldName': [listOfErrors], 'otherFieldName': [listOfErrors]}
             let field_errors = data.errors
 
-            if ("course_code" in Object.keys(field_errors)) {
+            if (field_errors.hasOwnProperty("course_code")) {
                 const courseCodeField_error = field_errors["course_code"][0]
 
-                const courseCodeErrorElem = document.getElementById("#course-code-error")
-                courseCodeErrorElem.textContent += courseCodeField_error
+                const courseCodeErrorElem = document.getElementById("course-code-error")
+                courseCodeErrorElem.style.display = 'inline';
+                courseCodeErrorElem.insertAdjacentText("beforeend", courseCodeField_error)
             }
-            if ("course_title" in Object.keys(field_errors)) {
+            if (field_errors.hasOwnProperty("course_title")) {
                 const courseTitle_error = field_errors["course_title"][0]
 
-                const courseTitleErrorElem = document.getElementById("#course-title-error")
-                courseTitleErrorElem.textContent += courseTitle_error
+                const courseTitleErrorElem = document.getElementById("course-title-error");
+                courseTitleErrorElem.style.display = 'inline';
+                courseTitleErrorElem.insertAdjacentText("beforeend", courseTitle_error)
             }
         } else {
             // status = 500
-            console.log("Error saving to db");
             const alertString = `<div class="alert alert-danger mt-4 mx-5 py-2 text-center fw-bold" role="alert">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:"> <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg> 
                                     <div>
@@ -107,10 +109,6 @@ document.addEventListener("DOMContentLoaded", function() {
     form_1.addEventListener('submit', (event) => {
         event.preventDefault();
         const formData = new FormData(form_1);
-        // console.log(JSON.stringify(formData));
-        // for (var [key, value] of formData.entries()) {
-        //     console.log(key, value);
-        // }
         submitForm(formData);
     });
 })
