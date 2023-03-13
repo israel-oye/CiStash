107196aa5dc5be29a02a430182969e7080975929
israel-oye <ioyeboade@gmail.com>
2023-03-13 16:46:17 +0100

    implement async course upload submission

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, flash, render_template, redirect, url_for, request
-from extensions import login_manager, current_user, login_user, login_required, db
+from flask import Blueprint, current_app, flash, render_template, redirect, url_for, request, jsonify
+from werkzeug.datastructures import ImmutableMultiDict
+from extensions import login_manager, current_user, login_user, login_required, logout_user, db
 from models.admin import Moderator
-from .form import RegisterForm
+from models.course import Course
+from .form import RegisterForm, CourseForm
 from passlib.hash import sha256_crypt
 
 auth_bp = Blueprint("auth_bp", __name__, template_folder="src/templates", static_folder="static")
@@ -26,26 +28,26 @@ def register():
         flash("Successfully registered. Please log in.", "success")
         return redirect(url_for("auth_bp.login"))
 
-
     return render_template("register.html", form=register_form)
 
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("auth_bp.index"))
+        return redirect(url_for("auth_bp.upload"))
     
     if request.method == "POST":
+        
         input_email = request.form.get("email")
         input_pwd = request.form.get("password")
         
         try:
-            user = Moderator.query.get_or_404(email=input_email)
-        except:
-            error = "Incorrect email/password combination"
+            user = Moderator.query.filter_by(email=input_email).first_or_404()
+        except Exception as e:
+            error = "User not found"
             return render_template("login.html", error=error)
         else:
-            if user.password_is_correct(sha256_crypt.encrypt(input_pwd)):
+            if user.password_is_correct(password_candidate=input_pwd):
                 flash("Login successful!", "success")
                 login_user(user=user)
                 return redirect(url_for("auth_bp.upload"))
@@ -56,8 +58,44 @@ def login():
 
     return render_template("login.html")
 
+@auth_bp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Successfully logged out", 'success')
+    return redirect(url_for("auth_bp.login"))
 
-@auth_bp.route("/upload", methods=["GET", "POST"])
+@auth_bp.route("/upload", methods=['GET'])
+@login_required
+def get_upload_page():
+    form = CourseForm()
+    form.dyna_course_code.choices = [(course.id_, course.course_code) for course in Course.query.order_by("course_code")]
+
+    return render_template("upload.html", form=form)
+
+@auth_bp.route("/upload", methods=["POST"])
+@login_required
+def upload():
+
+    form = CourseForm(data=request.get_json())
+    
+    if form.validate_on_submit():
+        new_course = Course(
+            course_title=str(form.course_title.data).title(),
+            course_code=form.course_code.data
+            )
+        try:
+            db.session.add(new_course)
+            db.session.commit()
+        except Exception as e:
+            return jsonify({"message": "An error occurred while saving, please try again."}), 500
+        return jsonify({"message": "Course added successfully", "redirect_url": "/pass"}), 200 #TODO probably add redirect link. 
+                                                                                               #"redirect_url": url_for(home.index)
+    else:
+        errors = form.errors
+        return jsonify({"errors": errors}), 400
+
+@auth_bp.route("/file_upload", methods=["POST"])
 @login_required
 def file_upload():
     ...
---
+++ src/static/assets/form-handler.js
@@ -0,0 +1,116 @@
+// import Dropzone from "dropzone";
+
+
+// Dropzone.options.upload = {
+//     paramName: "file",
+//     acceptedFiles: ".pdf, .doc, .docx, .odt, .ppt, .pptx",
+//     addRemoveLinks: true,
+//     chunking: true,
+//     forceChunking: true,
+//     url: document.querySelector("#upload-url").value,
+//     uploadMultiple: false,
+//     maxFilesize: 1025, // megabytes
+//     chunkSize: 1000000, // bytes
+//     dictDefaultMessage: "Upload relevant course document.",
+//     dictCancelUpload: "Cancel Upload",
+//     dictCancelUploadConfirmation: "Are you sure you want to cancel this upload?",
+//     init: function() {
+//         this.on('error', function(file, errorMessage) {
+//             if (file.accepted) {
+//                 var mypreview = document.getElementsByClassName('dz-error');
+//                 mypreview = mypreview[mypreview.length - 1];
+//                 mypreview.classList.toggle('dz-error');
+//                 mypreview.classList.toggle('dz-success');
+//             }
+//         });
+//     },
+
+// }
+
+async function submitForm(formData) {
+    try {
+        const response = await fetch('/auth/upload', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': formData.get('csrf_token')
+            },
+            body: JSON.stringify({
+                    "csrf_token": formData.get("csrf_token"),
+                    "levels": formData.get("levels"),
+                    "course_code": formData.get("course_code"),
+                    "course_title": formData.get("course_title"),
+                })
+                // body: JSON.stringify(formData)
+        });
+
+        const data = await response.json();
+
+        if (response.status === 200) {
+            const alertString = `<div class="alert alert-success mt-4 mx-5 py-2 text-center fw-bold" role="alert">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:"> <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg> 
+                                    <div>
+                                        ${data.message}
+                                    </div>
+                                </div>`
+
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(alertString, "text/html");
+            const alertElem = doc.querySelector('div')
+            const formContainerElem = doc.querySelector('#form-container')
+
+            formContainerElem.insertAdjacentElement("beforebegin", alertElem);
+        } else if (response.status === 400) {
+            //data = resp => {'errors': {'fieldName': [listOfErrors]}}
+
+            //field_errors = {'fieldName': [listOfErrors], 'otherFieldName': [listOfErrors]}
+            let field_errors = data.errors
+
+            if ("course_code" in Object.keys(field_errors)) {
+                const courseCodeField_error = field_errors["course_code"][0]
+
+                const courseCodeErrorElem = document.getElementById("#course-code-error")
+                courseCodeErrorElem.textContent += courseCodeField_error
+            }
+            if ("course_title" in Object.keys(field_errors)) {
+                const courseTitle_error = field_errors["course_title"][0]
+
+                const courseTitleErrorElem = document.getElementById("#course-title-error")
+                courseTitleErrorElem.textContent += courseTitle_error
+            }
+        } else {
+            // status = 500
+            console.log("Error saving to db");
+            const alertString = `<div class="alert alert-danger mt-4 mx-5 py-2 text-center fw-bold" role="alert">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:"> <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/> </svg> 
+                                    <div>
+                                        ${data.message}
+                                    </div>
+                                </div>`
+
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(alertString, "text/html");
+            const alertElem = doc.querySelector('div')
+            const formContainerElem = doc.querySelector('#form-container')
+
+            formContainerElem.insertAdjacentElement("beforebegin", alertElem);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form_1 = document.getElementById("form1");
+
+    form_1.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(form_1);
+        // console.log(JSON.stringify(formData));
+        // for (var [key, value] of formData.entries()) {
+        //     console.log(key, value);
+        // }
+        submitForm(formData);
+    });
+})
