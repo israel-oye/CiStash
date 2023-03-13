d505fb7e2835e0092061796681d904c775de2115
israel-oye <ioyeboade@gmail.com>
2023-03-13 21:50:55 +0100

    add extra validation to course_title and selected_level fields

--- src/auth/form.py
+++ src/auth/form.py
@@ -44,7 +44,7 @@ class CourseForm(FlaskForm):
                         )
 
     def validate_levels(self, selected_level):
-        if selected_level.data not in [l.value for l in LevelEnum]:
+        if selected_level.data not in [l.name for l in LevelEnum]:
             raise ValidationError("Please select a valid level.")
     
     def validate_course_code(self, course_code_field):
@@ -52,6 +52,8 @@ class CourseForm(FlaskForm):
             raise ValidationError(f"Oops, {course_code_field.data} has been registered.")
         
     def validate_course_title(self, course_title_field):
-        if Course.query.filter_by(course_title=course_title_field.data).first():
+        field_data = str(course_title_field.data).title()
+        
+        if Course.query.filter_by(course_title=field_data).first():
             raise ValidationError(f"It seems like this course has been registered, please check carefully and try again.")
     
