3b971f40b03972cb8627bf7c9931ad152480e470
israel-oye <ioyeboade@gmail.com>
2023-03-13 22:12:28 +0100

    add unique constraint to Course model

--- models/course.py
+++ models/course.py
@@ -4,7 +4,7 @@ class Course(db.Model):
     __tablename__ = "course"
 
     id_ = db.Column(db.Integer, primary_key=True)
-    course_title = db.Column(db.String(60))
+    course_title = db.Column(db.String(60), unique=True)
     course_code = db.Column(db.String(20), unique=True, nullable=False)
     # course_docs = db.relationship("document", back_populates="course")
     # course_level = db.relationship("level", back_populates="courses")
