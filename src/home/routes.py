9c9e3a366be31b39d1fbef4adf65c4bd6c3fd04c
israel-oye <ioyeboade@gmail.com>
2023-04-04 21:00:03 +0100

    add level pages

--- src/home/routes.py
+++ src/home/routes.py
@@ -1,6 +1,6 @@
 from typing import SupportsInt
 from flask import current_app, flash, redirect, request, render_template, url_for, Blueprint
-from models.level import Level
+from models.level import Level, LevelEnum
 
 home_bp = Blueprint('home_bp', __name__, template_folder="src/templates", static_folder="src/static")
 
@@ -10,26 +10,13 @@ def page_not_found(error):
     msg = "404 Error: Page Not Found. It looks like you're lost, but don't worry. Try using the search bar or returning to the homepage to find what you're looking for."
     return render_template("errors/404.html", msg=msg), 404
 
+
 @home_bp.get("/")
 def index():
-    # current_app.logger.info(request.path.split('/'))
-    lvls = [
-        {'id': 1, 'name': 100},
-        {'id': 2, 'name': 200},
-        {'id': 3, 'name': 300},
-        {'id': 4, 'name': 400}
-    ]
-    # levels = Level.query.all()
-    return render_template("index.html", levels=lvls)
+    return render_template("index.html")
 
-@home_bp.get("/100")
-def linker():
-    return "<h3>Wagwan</h3>"
-# def get_segment(request): 
-#     try:
-#         segment = request.path.split('/')[-1]
-#         if segment == '':
-#             segment = 'index'
-#         return segment    
-#     except:
-#         return None
+
+@home_bp.get("/level/<int:level_id>")
+def level_page(level_id):
+    level = Level.query.filter_by(id_=level_id).first()
+    return render_template("level_page.html", level=level)
