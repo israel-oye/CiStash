8652fa231cc65c2e6dbeb77753d81ea85256e0b9
israel-oye <ioyeboade@gmail.com>
2023-04-04 21:01:20 +0100

    persist levels to db and add level context to * templates

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,8 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from extensions import db, migrate, crsf, login_manager, admin
-from models.moderator import Moderator, IndexView, ModeratorView
-from models.course import Course, CourseView
-from models.doc import Document, DocumentView
+from models.moderator import Moderator, IndexView
+from models.level import Level, LevelEnum
 
 
 from .home import home_bp
@@ -21,11 +20,27 @@ crsf.init_app(app)
 db.init_app(app)
 migrate.init_app(app, db, render_as_batch=True)
 login_manager.init_app(app)
-admin.init_app(app, index_view=IndexView(name='Admin',url="/auth/admin") )
+admin.init_app(app, index_view=IndexView(name='Admin',url="/auth/admin"))
 
 
 with app.app_context():
     db.create_all()
+
+    levels = []
+    for l in LevelEnum:
+        new_level = Level(name=l.name)
+        levels.append(new_level)
+
+    try:
+        db.session.add_all(levels)
+        db.session.commit()
+    except Exception as e:
+        pass
+    
+@app.context_processor
+def inject_levels():
+    lvls = {level.name: level.value for level in LevelEnum}
+    return dict(lvl_mapping=lvls)
 
 @login_manager.user_loader
 def load_user(user_id):
