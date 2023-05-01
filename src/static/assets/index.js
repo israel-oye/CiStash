dc75312d507722f5558da319c888bf958cff31e5
israel-oye <ioyeboade@gmail.com>
2023-05-01 07:51:28 +0100

    Make more cosmetic changes and add font

--- src/static/assets/index.js
+++ src/static/assets/index.js
@@ -2,11 +2,13 @@ let values = ['Upload', `<i class=" text-primary fa-solid fa-upload fa-bounce fa
 let currentIndex = 0;
 let uploadElem = document.querySelector('#upload-nav');
 
-setInterval(() => {
-    uploadElem.innerHTML = values[currentIndex];
-    currentIndex++;
-    if (currentIndex === 3) {
-        currentIndex = 0;
-    }
+if (uploadElem) {
+    setInterval(() => {
+        uploadElem.innerHTML = values[currentIndex];
+        currentIndex++;
+        if (currentIndex === 3) {
+            currentIndex = 0;
+        }
 
-}, 10000)
+    }, 10000)
+}
