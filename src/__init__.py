78f438ba5a5e48aa842d2390a598743e242ca7bd
israel-oye <ioyeboade@gmail.com>
2023-10-07 11:50:36 +0100

    Set application factory pattern

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,6 @@
-import os
-from datetime import timedelta
-from dotenv import load_dotenv
 from flask import Flask, session
 
-from config import DevelopmentConfig
+from config import DevelopmentConfig, ProductionConfig, TestingConfig
 from extensions import admin, crsf, db, login_manager, mail, migrate, IntegrityError
 from models.level import Level, LevelEnum
 from models.moderator import IndexView, Moderator
@@ -12,53 +9,57 @@ from .auth import auth_bp
 from .home import home_bp
 from .resource import resource_bp
 
-load_dotenv()
 
-app = Flask(__name__)
+def initialize_extensions(app: Flask):
+    crsf.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
+    login_manager.init_app(app)
+    admin.init_app(app, index_view=IndexView(name='Admin', url="/auth/admin"))
+    mail.init_app(app)
 
-app.config.from_object(DevelopmentConfig)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Moderator.query.get(int(user_id))
 
-crsf.init_app(app)
-db.init_app(app)
-migrate.init_app(app, db, render_as_batch=True)
-login_manager.init_app(app)
-admin.init_app(app, index_view=IndexView(name='Admin', url="/auth/admin"))
-mail.init_app(app)
+def register_blueprints(app: Flask):
+    app.register_blueprint(home_bp, url_prefix="/home")
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(resource_bp, url_prefix="/resource")
 
-with app.app_context():
-    db.create_all()
+def create_app(config_filename=None):
+    app = Flask(__name__)
 
-    levels = []
-    for l in LevelEnum:
-        new_level = Level(name=l.name)
-        levels.append(new_level)
+    app.config.from_object(TestingConfig)
 
-    try:
-        db.session.add_all(levels)
-        db.session.commit()
-    except IntegrityError as e:
-        db.session.rollback()
+    initialize_extensions(app)
+    register_blueprints(app)
 
-@app.before_request
-def make_session_permanent():
-    session.permanent = True
-    app.permanent_session_lifetime = timedelta(days=2, hours=12)
+    with app.app_context():
+        db.create_all()
 
-@app.context_processor
-def utility_processor():
-    def get_file_format(file_name: str):
-        return file_name.split('.')[-1]
+        levels = []
+        for l in LevelEnum:
+            new_level = Level(name=l.name)
+            levels.append(new_level)
 
-    lvls = {level.name: level.value for level in LevelEnum}
-    return dict(get_file_format=get_file_format, lvl_mapping=lvls)
+        try:
+            db.session.add_all(levels)
+            db.session.commit()
+        except IntegrityError as e:
+            db.session.rollback()
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Moderator.query.get(int(user_id))
+    @app.before_request
+    def make_session_permanent():
+        session.permanent = True
 
-app.register_blueprint(home_bp, url_prefix="/home")
-app.register_blueprint(auth_bp, url_prefix="/auth")
-app.register_blueprint(resource_bp, url_prefix="/resource")
+    @app.context_processor
+    def utility_processor():
+        def get_file_format(file_name: str):
+            return file_name.split('.')[-1]
 
-if __name__ == "__main__":
-    app.run()
+        lvls = {level.name: level.value for level in LevelEnum}
+        return dict(get_file_format=get_file_format, lvl_mapping=lvls)
+
+
+    return app
