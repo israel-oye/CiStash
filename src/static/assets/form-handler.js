f7d41dbd84aec47a515a9dfcae76cb7de3726b0d
israel-oye <ioyeboade@gmail.com>
2023-10-14 05:27:43 +0100

    DEBUG: Disable force-chunking

--- src/static/assets/form-handler.js
+++ src/static/assets/form-handler.js
@@ -14,7 +14,7 @@ Dropzone.options.upload = {
     maxFiles: 1,
     maxFilesize: 512, // megabytes
     chunkSize: 1 * 1024 * 1024, // bytes
-    retryChunks: true,
+    retryChunks: false,
     retryChunksLimit: 3,
     dictDefaultMessage: '<h5>Drag file here or click to upload relevant course document. <i style="display: block;" class="fa-solid fa-2x fa-cloud-arrow-up"></i></h5>',
     dictCancelUpload: "Cancel Upload",
